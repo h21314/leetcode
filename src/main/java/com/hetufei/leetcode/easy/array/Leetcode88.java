b3710c65f45bea38af33c64a94ff23149d28a641
hekangmin <1451478969@qq.com>
2021-03-30 23:19:45 +0800

    希尔排序

---
+++ src/main/java/com/hetufei/leetcode/easy/array/Leetcode88.java
@@ -0,0 +1,57 @@
+package com.hetufei.leetcode.easy.array;
+//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+//
+// 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nu
+//ms2 的元素。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//输出：[1,2,2,3,5,6]
+//
+//
+// 示例 2：
+//
+//
+//输入：nums1 = [1], m = 1, nums2 = [], n = 0
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// nums1.length == m + n
+// nums2.length == n
+// 0 <= m, n <= 200
+// 1 <= m + n <= 200
+// -109 <= nums1[i], nums2[i] <= 109
+//
+// Related Topics 数组 双指针
+// 👍 808 👎 0
+/**
+ * @Author: {USER}
+ * @Date: {DATE} TIME}
+ * @Description: TODO
+ **/
+public class Leetcode88 {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (nums2.length == 0) {
+            return;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            int temp = nums1[i];
+            for (int j = n - 1; j >= 0; j--) {
+                //说明此时nums2[j]是最大的
+                if (nums2[j] >= temp) {
+
+                }
+            }
+        }
+    }
+}
