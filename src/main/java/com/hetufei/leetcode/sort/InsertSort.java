da6edf33a52417b49de26d27a8fd59ac9c24c773
hekangmin <hekangmin@qtrade.com.cn>
2021-03-30 18:07:30 +0800

    1、插入排序实现

--- src/main/java/com/hetufei/leetcode/sort/InsertSort.java
+++ src/main/java/com/hetufei/leetcode/sort/InsertSort.java
@@ -6,6 +6,26 @@ package com.hetufei.leetcode.sort;
  */
 public class InsertSort {
     public static int[] insertSort(int[] array) {
-        return null;
+        if (array.length <= 1) {
+            return array;
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            int current = array[i + 1];
+            int index = i;
+            while (index >= 0 && current < array[index]) {
+                array[index + 1] = array[index];
+                index--;
+            }
+            array[index + 1] = current;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2,3,1,5,0};
+        int[] result = insertSort(array);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(i);
+        }
     }
 }
