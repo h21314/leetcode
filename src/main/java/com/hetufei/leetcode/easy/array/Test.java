6f90d1638e7e8938f0d5b41833e3cc4570eb473f
hekangmin <hekangmin@qtrade.com.cn>
2021-04-12 16:41:15 +0800

    1、add sout

--- src/main/java/com/hetufei/leetcode/easy/array/Test.java
+++ src/main/java/com/hetufei/leetcode/easy/array/Test.java
@@ -5,4 +5,7 @@ package com.hetufei.leetcode.easy.array;
  * Created by hekangmin on 2021/4/12 16:40
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("-------");
+    }
 }
